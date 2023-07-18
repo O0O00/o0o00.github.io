0d81a6ca5123b15c02c92cddc43b5ddac4827430
O0O00 <48128321+O0O00@users.noreply.github.com>
2023-07-18 15:02:58 +0900

    Add files via upload

--- script/index.js
+++ script/index.js
@@ -78,10 +78,7 @@ function onselectdep(){
     console.log(dom);
     $('#subject').append(dom);
 }
-function sleep(ms) {
-    const wakeUpTime = Date.now() + ms;
-    while (Date.now() < wakeUpTime) {}
-  }
+
 function search(){
     $('.listview').empty();
     let dat = $("#subject option:checked").val();
@@ -91,23 +88,22 @@ function search(){
 
     //request data
     $.post("http://40.82.159.60:8080/getList",{},(res, status)=> {
-    let validSub = [];
-       res = res[dat[0]];
-       for(i in res){
-           if(res[i].ws == dat[3]) validSub.push(res[i]);
-       }
-       let dom = '';
+        let validSub = [];
+        res = res[dat[0]];
+        for(i in res){
+            if(res[i].ws == dat[3]) validSub.push(res[i]);
+        }
+        let dom = '';
+        if(validSub == []){
+            dom = `<div class="list""><div class="item" id=":(">슬프게도 해당 과목에 대한 요청이 없습니다.</div></div>`
+            $('.listview').append(dom);
+        }
        for(i in validSub){
            i = validSub[i];
            dom = `<div class="list" id="${i.sid}/${i.ws}/${i.ts}" onclick= "listclickevent(this)"><div class="item" id="reg">${i.ws}</div><div class="item" id="hop">${i.ts}</div><div class="item" id="id">${i.sid}</div></div>`
            $('.listview').append(dom);
        }
-
-
-
     });
-    //filtering
-    //build dom
 }
 
 
